439fc1e4a8d3c66492d275bdd2104fac31b74095
User <User@DESKTOP-2EGH8KJ>
2022-10-18 16:28:51 +0200

    Add project files.

---
+++ DH8G3K_HFT_2022231.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DH8G3K_HFT_2022231.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ DH8G3K_HFT_2022231.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DH8G3K_HFT_2022231.Models
+{
+    public class Class1
+    {
+    }
+}
