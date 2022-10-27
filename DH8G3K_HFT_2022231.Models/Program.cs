98118749084723493a679676c4e063da0d597f4b
Füzér Levente <fuzerl@stud.uni-obuda.hu>
2022-10-27 17:09:51 +0200

    created Program.cs in Models

---
+++ DH8G3K_HFT_2022231.Models/Program.cs
@@ -0,0 +1,19 @@
+﻿using DH8G3K_HFT_2022231.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DH8G3K_HFT_2022231.Models
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VideogamesDbContext ctx = new VideogamesDbContext();
+
+            ctx.Videogames.ToList().ForEach(t => Console.WriteLine(t.Title));
+        }
+    }
+}
