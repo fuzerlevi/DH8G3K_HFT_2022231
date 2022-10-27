3d31b0b27629b02b0601f9f7bd9ae9982f9820f5
Füzér Levente <fuzerl@stud.uni-obuda.hu>
2022-10-27 15:23:45 +0200

    DbSets added to VideogamesDbContext

---
+++ DH8G3K_HFT_2022231.Models/Data/VideogamesDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DH8G3K_HFT_2022231.Models.Data
+{
+    class VideogamesDbContext : DbContext
+    {
+        public DbSet<Videogame> Videogames { get; set; }
+        public DbSet<Developer> Developers { get; set; }
+        public DbSet<Franchise> Franchises { get; set; }
+
+        
+    }
+}
