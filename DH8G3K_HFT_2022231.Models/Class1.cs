7027320de91c96056f2687e92dc6d2ea572e93bb
Füzér Levente <fuzerl@stud.uni-obuda.hu>
2022-10-27 15:05:39 +0200

    data tables created and added to Models folder

--- DH8G3K_HFT_2022231.Models/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace DH8G3K_HFT_2022231.Models
-{
-    public class Class1
-    {
-    }
-}
---
+++ DH8G3K_HFT_2022231.Models/Models/Developer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DH8G3K_HFT_2022231.Models
+{
+    class Developer
+    {
+        
+    }
+}
