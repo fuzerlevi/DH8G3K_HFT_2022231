af91955d7a5d5d472dfefdae63e46f81b2960b4b
Füzér Levente <fuzerl@stud.uni-obuda.hu>
2022-11-29 21:04:12 +0100

    edited Startup.cs

--- DH8G3K_HFT_2022231.Endpoint/Startup.cs
+++ DH8G3K_HFT_2022231.Endpoint/Startup.cs
@@ -3,6 +3,7 @@ using DH8G3K_HFT_2022231.Models;
 using DH8G3K_HFT_2022231.Repository;
 using DH8G3K_HFT_2022231.Repository.Database;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -21,7 +22,7 @@ namespace DH8G3K_HFT_2022231.Endpoint
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<VideogameDbContext>();
+            services.AddSingleton<VideogameDbContext>();
 
             services.AddTransient<IRepository<Videogame>, VideogameRepository>();
             services.AddTransient<IRepository<Developer>, DeveloperRepository>();
@@ -44,16 +45,26 @@ namespace DH8G3K_HFT_2022231.Endpoint
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DH8G3K_HFT_2022231.Endpoint v1"));
             }
+
+            app.UseExceptionHandler(c => c.Run(async context =>
+            {
+                var exception = context.Features
+                    .Get<IExceptionHandlerPathFeature>()
+                    .Error;
+                var response = new { Msg = exception.Message };
+                await context.Response.WriteAsJsonAsync(response);
+            }));
 
             app.UseRouting();
 
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
